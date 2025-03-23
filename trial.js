a7ac30c712b25f2b5c1fbe9764e92b932a3fc3a0
pratikjadhav0908 <159643367+pratikjadhav0908@users.noreply.github.com>
2025-03-23 22:14:52 +0530

    Delete trial.js

--- trial.js
+++
@@ -1,9 +0,0 @@
-let opts=document.querySelector("#select")
-let selects=document.querySelector("#lawid")
-opts.addEventListener("change", ()=>{
-    const opt= opts.options[opts.selectedIndex];
-    if(opt.value==="2"){
-        selects.style="display:block";
-        console.log("selected");
-    }
-})
